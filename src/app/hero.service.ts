7932fe99787b379f2ee2150cfa4109f818339cb5
Shan <shan.azizh@gmail.com>
2021-05-19 20:32:05 +0200

    Step: Add Services Ajout de message service (Injecter dans HeroService)  et affichage des messages

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -9,10 +10,11 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
   	const heroes = of(HEROES);
+  	this.messageService.add('HeroService: fetched heroes');
   	return heroes;
   }
 }
